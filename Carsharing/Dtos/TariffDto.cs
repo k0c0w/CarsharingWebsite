1d4d0702e65b3bf334b69dd0c39ec8b2f7227bd4
EgorovIMPL <egorov.r.684017@gmail.com>
2023-03-26 22:20:24 +0300

    added TariffDto and TariffController

---
+++ Carsharing/Dtos/TariffDto.cs
@@ -0,0 +1,9 @@
+namespace Carsharing.Forms;
+
+public class TariffDto
+{
+    public string? Name { get; set; }
+    public int Price { get; set; }
+    public string? Period { get; set; }
+    public string? Description { get; set; }
+}
