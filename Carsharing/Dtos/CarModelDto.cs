5dbbc75e1c909e59068a68da76aa1abff5120aa4
EgorovIMPL <egorov.r.684017@gmail.com>
2023-03-26 22:20:59 +0300

    added CarModelDto and CarModelController

---
+++ Carsharing/Dtos/CarModelDto.cs
@@ -0,0 +1,14 @@
+using Carsharing.Model;
+
+namespace Carsharing.Forms;
+
+public class CarModelDto
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string SourceImg { get; set; }
+
+    public int TariffId { get; set; }
+}
