1a278ce780220acf8b8f683dae2b37e4e43b8e02
EgorovIMPL <egorov.r.684017@gmail.com>
2023-03-26 17:07:39 +0300

    Added ClientController with HttpPost methods Registration and Login

---
+++ Carsharing/Controllers/ClientController.cs
@@ -0,0 +1,66 @@
+﻿using Carsharing.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Carsharing.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Carsharing.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClientController : Controller
+{
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Registration([FromBody]RegistrationForm form)
+    {
+        if (form.Password != form.RetryPassword)
+        {
+            return BadRequest("Wrong confirm password");
+        }
+        await using (var context = new CarsharingContext())
+        {
+            var client = context.Clients.FirstOrDefaultAsync(cl => cl.Email == form.Email);
+            if (client.Result!.Email == form.Email)
+            {
+                return BadRequest("A client with this email already exists");
+            }
+            var id = context.Clients.Count();
+            var newClient = 
+                new Client
+                {
+                    Id = id + 1,
+                    Email = form.Email!, 
+                    ClientInfo = new ClientInfo
+                    {
+                        Name = form.Name!, 
+                        Surname = form.Surname!, 
+                        Age = form.Age,
+                        ClientId = id, 
+                        PassportType = "passport"
+                    },
+                    Password = form.Password!,
+                    RoleId = 1 //id for user
+                };
+            await context.AddAsync(newClient);
+            await context.SaveChangesAsync();
+        }
+        return Ok();
+    }
+    
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Login([FromBody]LoginForm form)
+    {
+        await using (var context = new CarsharingContext())
+        {
+            var client = context.Clients.FirstOrDefaultAsync(cl => cl.Email == form.Email);
+            if (client.Result == null)
+            {
+                return Unauthorized("There is no client with this email");
+            }
+            if (client.Result.Password != form.Password)
+            {
+                return Unauthorized("Wrong password");
+            }
+        }
+        return Ok();
+    }
+}
