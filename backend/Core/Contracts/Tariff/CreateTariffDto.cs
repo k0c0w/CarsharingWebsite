2821ee4c969e52385d49d262ce81eb4059f9e0da
k0c0w <107144084+k0c0w@users.noreply.github.com>
2023-05-08 18:14:11 +0300

    Tariff service implementation
    * created service signature
    
    * created some transfer objects
    
    * created new exception
    
    * implemented tariff service
    
    * registering service. Created explicit default model validator behavior to change it in fututre
    
    * moved controllers, created some tariff controllers
    
    * deleted folder
    
    * implemented admin tariff endpoint
    
    * moved controllers t another folder
    
    * fullfilled tariff controllers

---
+++ backend/Core/Contracts/Tariff/CreateTariffDto.cs
@@ -0,0 +1,12 @@
+namespace Contracts.Tariff;
+
+public class CreateTariffDto
+{
+    public string Name { get; init; }
+
+    public string Description { get; init; }
+    
+    public decimal PriceInRubles { get; init; }
+    
+    public int? MaxMileage { get; init; }
+}
