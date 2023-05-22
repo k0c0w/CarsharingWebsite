ebcc4920fc58d97b5a677c3a5b368971980f0e80
Artur Yakupov <art.kazan@mail.ru>
2023-05-22 16:20:04 +0300

    deleted unrelated files

--- src/data/carsRows.js
+++
@@ -1,9 +0,0 @@
-export const rows = [
-    { name: 'merseders', number: 159, id: 6, tarrif:'5H', func:"lol" },
-    { name: 'lexus', number: 237, id: 9, tarrif:'1W', func:"lol" },
-    { name: 'toyota', number: 262, id: 16, tarrif:'5H', func:"lol" },
-    { name: 'kalina', number: 305, id: 3, tarrif:'5H', func:"lol" },
-    { name: 'porsha', number: 356, id: 19, tarrif:'5H', func:"lol" },
-];
-
-
--- src/data/tarrifsRows.js
+++
@@ -1,9 +0,0 @@
-export const rows = [
-    { name: 'ball', price: 159.000, id: 6, period:'5H' },
-    { name: 'lol', price: 237.100, id: 9, period:'1W' },
-    { name: 'hllllhohooh', price: 262.000, id: 16, period:'5H' },
-    { name: 'kala', price: 305.000, id: 3, period:'5H' },
-    { name: 'Trans', price: 35.000, id: 19, period:'5H' },
-];
-
-
