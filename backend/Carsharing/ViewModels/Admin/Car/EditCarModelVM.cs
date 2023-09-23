ea2f395b49b7b8582d68ab2e584948256de7a442
ValyaGrachov <goust190703@gmail.com>
2023-09-24 00:54:17 +0300

    Destroy warnings

--- backend/Carsharing/ViewModels/Admin/Car/EditCarModelVM.cs
+++ backend/Carsharing/ViewModels/Admin/Car/EditCarModelVM.cs
@@ -4,7 +4,7 @@ namespace Carsharing.ViewModels.Admin.Car;
 
 public record EditCarModelVM
 {
-    //todo:
+ 
     public string? Description { get; init; }
     [ImageValidator(5000)]
     public IFormFile? Image { get; init; }
--- backend/Carsharing/ViewModels/Profile/UserInfoVM.cs
+++ backend/Carsharing/ViewModels/Profile/UserInfoVM.cs
@@ -5,10 +5,10 @@ namespace Carsharing.ViewModels.Profile;
 public record UserInfoVM
 {
     [JsonPropertyName("email")]
-    public string Email { get; init; }
+    public string? Email { get; init; }
     
     [JsonPropertyName("full_name")]
-    public string FullName { get; init; }
+    public string? FullName { get; init; }
     
     [JsonPropertyName("balance")]
     public decimal Balance { get; init; }
