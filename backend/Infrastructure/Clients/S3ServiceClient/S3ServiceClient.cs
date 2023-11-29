6282aeed886693ebedefcaac98fe8cdc788d5071
Artur Yakupov <art.kazan@mail.ru>
2023-11-29 20:56:10 +0300

    Clients drafts

---
+++ backend/Infrastructure/Clients/S3ServiceClient/S3ServiceClient.cs
@@ -0,0 +1,54 @@
+﻿using Clients.Objects;
+using System.Net.Http.Json;
+
+namespace Clients.S3ServiceClient
+{
+    public class S3ServiceClient : ClientBase
+    {
+        public S3ServiceClient(HttpClient httpClient) : base("https://localhost:8000", httpClient)
+        {
+        }
+
+        public async Task<WebCallResult<string>> CreateFileAsync(string bucketName, string fileName, Stream bytes, string contentType)
+        {
+            if (!ValidContentType(contentType))
+                return new WebCallResult<string>(new ArgumentError($"Unsupported Content-Type: {contentType}"));
+
+            var request = CreateRequestMessage(HttpMethod.Post, $"files/{bucketName}/{fileName}");
+            request.Content = new StreamContent(bytes);
+            request.Headers.Add("Content-Type", contentType);
+
+            try
+            {
+                var response = await HttpClient.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var accessLink = await response.Content.ReadAsStringAsync();
+                    return new WebCallResult<string>(response.StatusCode, response.Headers, default, default, request.RequestUri.AbsolutePath, default, HttpMethod.Post,default, accessLink, default);
+                }
+
+                var error = await response.Content.ReadFromJsonAsync();
+
+                return new WebCallResult<string>(response.StatusCode, response.Headers, default, default, request.RequestUri.AbsolutePath, default, HttpMethod.Post, default, default, 
+                    new ServerError(error));
+            }
+            catch(HttpRequestException ex)
+            {
+                return new WebCallResult<string>(new CantConnectError());
+            }
+        }
+
+        private bool ValidContentType(string contentType)
+        {
+            return contentType switch
+            {
+                "image/png" => true,
+                "image/jpeg" => true,
+                _ => false,
+            };
+        }
+
+        //todo: validate bucket and filename
+    }
+}
---
+++ backend/Infrastructure/Clients/S3ServiceClient/UploadFileResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients.S3ServiceClient
+{
+    public record class UploadFileResponse
+    {
+
+        public string Message { get; set; }
+    }
+}
