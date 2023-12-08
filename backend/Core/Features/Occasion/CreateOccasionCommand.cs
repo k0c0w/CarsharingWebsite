249a3a3183fdf7f8222241dc3946c3d63f4ea888
Artur Yakupov <art.kazan@mail.ru>
2023-12-08 08:56:12 +0300

    .

---
+++ backend/Core/Features/Occasion/CreateOccasionCommand.cs
@@ -0,0 +1,13 @@
+﻿using Shared.CQRS;
+using Shared.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.Occasion;
+
+internal class CreateOccasionCommand : ICommand<Result<Guid>>
+{
+}
