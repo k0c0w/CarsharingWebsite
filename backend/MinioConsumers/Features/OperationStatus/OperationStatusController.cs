fcfe7e55ae6b66116f42f3be2096f8ea4aab7a79
Artur Yakupov <art.kazan@mail.ru>
2023-12-10 10:47:19 +0300

    Added operation status controller

---
+++ backend/MinioConsumers/Features/OperationStatus/OperationStatusController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MinioConsumer.Services.Repositories;
+using System.Security.Claims;
+
+namespace MinioConsumer.Features.OperationStatus;
+
+[Route("operation")]
+[Authorize]
+public class OperationStatusController : ControllerBase
+{
+    private readonly OperationRepository _operationRepository;
+    private readonly ILogger<Exception> _logger;
+
+    public OperationStatusController(OperationRepository repository, ILogger<Exception> logger)
+    {
+        _operationRepository = repository;
+        _logger = logger;
+    }
+
+    [HttpGet("{guid:guid}/status")]
+    public async Task<IActionResult> GetOperationStatusAsync([FromRoute] Guid guid)
+    {
+        if (!Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out Guid applicantId))
+            return BadRequest();
+        try
+        {
+            var operation = await _operationRepository.GetByIdAsync(guid);
+            if (operation is null || operation.InitializerUserId != applicantId)
+                return NotFound();
+
+            return new JsonResult(new 
+            {
+                status = operation.OperationStatus.ToString()
+            });
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            return NotFound();
+        }
+    }
+}
