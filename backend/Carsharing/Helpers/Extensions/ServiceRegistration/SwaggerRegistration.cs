08c666455ca06d955ae3406a04b505adf7474017
ruspyaloneworker <DioBrando2003@yandex.ru>
2023-12-17 15:46:49 +0300

    Допилил сохранение истории. Сделал пару эндпоинтов для облегчения проверки

--- backend/Carsharing/Helpers/Extensions/ServiceRegistration/SwaggerRegistration.cs
+++ backend/Carsharing/Helpers/Extensions/ServiceRegistration/SwaggerRegistration.cs
@@ -1,10 +1,34 @@
-﻿namespace Carsharing.Helpers.Extensions.ServiceRegistration;
+﻿using Microsoft.OpenApi.Models;
+
+namespace Carsharing.Helpers.Extensions.ServiceRegistration;
 
 public static class SwaggerRegistration
 {
     public static IServiceCollection RegisterSwagger(this IServiceCollection services)
     {
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            })
+            ;
         services.AddEndpointsApiExplorer();
 
         return services;
--- backend/MinioConsumers/Program.cs
+++ backend/MinioConsumers/Program.cs
@@ -34,7 +34,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    
 }
 #endregion
 
