7bf6b43b83913d34f1fb6be68b3c26a360881bc2
Artur Yakupov <art.kazan@mail.ru>
2024-03-27 10:40:18 +0300

    created empty flutter project

---
+++ mobileapp/android/app/src/main/kotlin/com/drive/mobileapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.drive.mobileapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mobileapp/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DriveApp());
+}
+
+class DriveApp extends StatelessWidget {
+  const DriveApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Drive',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+    );
+  }
+}
