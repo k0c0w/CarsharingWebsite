fc77fca373eb3ed3f60e23565caafa8ef5f9388f
ruspyaloneworker <DioBrando2003@yandex.ru>
2024-04-05 23:09:49 +0300

    Добавил subpage.

--- mobileapp/lib/profile.dart
+++ mobileapp/lib/profile.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'Components/appbar.dart';
+import 'Components/form_input_subpage.dart';
 import 'Components/styles.dart';
 
 class Profile extends StatelessWidget {
@@ -53,14 +54,22 @@ class Balance extends StatelessWidget {
 class InformationTable extends StatelessWidget {
   const InformationTable({super.key});
 
-  static createInformationField (String name, String value, { bool isError = false }) {
-    return Container(margin:  const EdgeInsets.only(left: 20.0, right: 20.0), child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(name, style: DriveTextStyles.inputLabel),
-        Text(value, style: isError ? DriveTextStyles.errorLabel : DriveTextStyles.userInput),
-      ],
-    ));
-  }
+  static createInformationField (
+      String name,
+      String value,
+      BuildContext context,
+      { Widget? subpage, bool isError = false }
+      )
+  => Scaffold(body:Text("lol"));
+  //TODO: Исправить
+  /// Закомментировал, чтобы убедиться, что ничего не рендерится
+  // => Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //       children: [
+  //       Text(name, style: DriveTextStyles.inputLabel),
+  //       TextButton(child: Text(value, style: isError ? DriveTextStyles.errorLabel : DriveTextStyles.userInput), onPressed: () => subpage != null ? Navigator.push(context, MaterialPageRoute(builder: (context) => subpage)) : null),
+  //     ],
+  //   );
+
 
   @override
   Widget build(BuildContext context) {
@@ -73,21 +82,68 @@ class InformationTable extends StatelessWidget {
 
     return Column(
         children: [
-          createInformationField("Номер телефона", "+ 7 (800) 555 35 35"),
+          createInformationField("Номер телефона", "+ 7 (800) 555 35 35", context,
+              subpage: DrivePhoneNumberInputSubpage(
+                initialValue: "+ 7 (800) 555 35 35",
+                inputTitle: "Номер телефона",
+                hintText: "",
+                onSavePressed: (String str) => {},
+              )),
           divider,
-          createInformationField("Почта", "example@mail.ru"),
+          createInformationField("Почта", "example@mail.ru", context,
+              subpage: DriveEmailInputSubpage(
+                initialValue: "example@mail.ru",
+                inputTitle: "Почта",
+                hintText: "",
+                onSavePressed: (String str) => {},
+              )),
           divider,
-          createInformationField("Имя", "Василий"),
+          createInformationField("Имя", "Василий", context,
+              subpage: DriveTextInputSubpage(
+                initialValue: "Василий",
+                inputTitle: "Отчество",
+                hintText: "",
+                onSavePressed: (String str) => {},
+              )),
           divider,
-          createInformationField("Фамилия", "Пупкин"),
+          createInformationField("Фамилия", "Пупкин", context,
+              subpage: DriveTextInputSubpage(
+                initialValue: "Пупкин",
+                inputTitle: "Отчество",
+                hintText: "",
+                onSavePressed: (String str) => {},
+              )),
           divider,
-          createInformationField("Отчество", "?", isError: true),
+          createInformationField("Отчество", "?", context, isError: true,
+              subpage: DriveTextInputSubpage(
+                initialValue: "?",
+                inputTitle: "Отчество",
+                hintText: "",
+                onSavePressed: (String str) => {},
+              )),
           divider,
-          createInformationField("Дата рождения", "27.02.2024"),
+          createInformationField("Дата рождения", "27.02.2024", context,
+              subpage: DriveDateInputSubpage(
+                inputTitle: "Дата рождения",
+                hintText: "",
+                onSavePressed: (DateTime date) => {},
+              )),
           divider,
-          createInformationField("Паспорт", "92 17 181511"),
+          createInformationField("Паспорт", "92 17 181511", context,
+              subpage: DriveTextInputSubpage(
+                initialValue: "92 17 181511",
+                inputTitle: "Паспорт",
+                hintText: "",
+                onSavePressed: (String str) => {},
+              )),
           divider,
-          createInformationField("ВУ", "5643-47820732"),
+          createInformationField("ВУ", "5643-47820732", context,
+              subpage: DriveTextInputSubpage(
+                initialValue: "5643-47820732",
+                inputTitle: "ВУ",
+                hintText: "",
+                onSavePressed: (String str) => {},
+              )),
         ]
     );
   }
