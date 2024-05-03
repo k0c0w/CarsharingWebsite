7f59f18294274ccc3b8b2e82785936a5511b81c7
Artur Yakupov <art.kazan@mail.ru>
2024-05-03 14:47:13 +0300

    added payment usecase

--- mobileapp/lib/domain/use_cases/payment_cases.dart
+++ mobileapp/lib/domain/use_cases/payment_cases.dart
@@ -1,11 +1,28 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:mobileapp/domain/results.dart';
 import 'package:mobileapp/domain/use_cases/base.dart';
 
-class PaymentUseCase extends UseCase<double> {
+class PaymentUseCase extends UseCase<bool> {
+  static const String _balanceMutation = """
+  mutation(\$value: Decimal!) {
+    increaseBalance(val: \$value)
+  }
+  """;
 
   Future<Result<bool>> call(double money) async {
+    final increaseBalanceMutationOptions = MutationOptions(
+        document: gql(_balanceMutation),
+        variables: {
+          "value": money
+        }
+    );
 
-    await Future.delayed(Duration(seconds: 5));
+    final increaseBalanceResult = await withTimeOut(graphQlClient.mutate(increaseBalanceMutationOptions));
+
+    if (increaseBalanceResult.hasException || isUnexecuted(increaseBalanceResult)) {
+      return tryDispatchError(increaseBalanceResult);
+    }
+
     return  Ok(true);
   }
 }
