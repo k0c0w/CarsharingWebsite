451849b02b98aa64bff9870bbfa95cacc2d39731
Artur Yakupov <art.kazan@mail.ru>
2024-05-06 11:49:26 +0300

    described chat proto service, created chat project

---
+++ backend/Chat.Microservice/Chat/Services/ChatService.cs
@@ -0,0 +1,20 @@
+﻿using ChatService;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Chat.Services;
+
+[Authorize]
+internal class ChatServiceImplementaion : ChatService.ChatService.ChatServiceBase
+{
+    public override Task GetChatStream(Empty request, IServerStreamWriter<FromServerMessage> responseStream, ServerCallContext context)
+    {
+        return base.GetChatStream(request, responseStream, context);
+    }
+
+    public override Task<Empty> SendMessage(FromClientMessage request, ServerCallContext context)
+    {
+        return base.SendMessage(request, context);
+    }
+}
